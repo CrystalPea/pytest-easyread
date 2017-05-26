3997dda0fbb7822f9ef06fd43e39ee06b6e2aa48
Pea Tyczynska <pea.tyczynska@digital.cabinet-office.gov.uk>
2017-05-26 16:34:48 +0100

    Change reporting behaviour to see if hook works

--- conftest.py
+++ conftest.py
@@ -1,5 +1,18 @@
 import pytest
 
-def answerIs42():
-    answer = 42
-    return answer == 42
+def is_it_answer_to_life_universe_and_everything(integer):
+    if isinstance(integer, int):
+        return integer == 42
+    else:
+        raise NameError("Hint: it's a number! :P")
+
+def pytest_report_teststatus(report):
+    import sys
+    sys.settrace(tracefunc)
+    if report.when in ("setup", "teardown"):
+        if report.failed:
+            return "negative", "NOPE" "INCORRECT"
+        elif report.skipped:
+            return "skipped", "s", "SKIPPED"
+        else:
+            return "positive", "SI", "correct"
--- test_my_plugin.py
+++ test_my_plugin.py
@@ -1,4 +1,7 @@
-from conftest import answerIs42
+from conftest import is_it_answer_to_life_universe_and_everything
 
-def testAnswerIs42():
-    assert answerIs42() == True
+def test_42_is_answer_to_life_universe_and_everything():
+    assert is_it_answer_to_life_universe_and_everything(42) == True
+
+def test_44is_not_answer_to_life_universe_and_everything():
+    assert is_it_answer_to_life_universe_and_everything("love") == False
