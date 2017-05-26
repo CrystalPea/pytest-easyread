556d04729fc31c656c9aea30fc54ceb72f6768e7
Pea Tyczynska <pea.tyczynska@digital.cabinet-office.gov.uk>
2017-05-26 16:36:13 +0100

    Set trace to find hook responsible for printing reports

--- conftest.py
+++ conftest.py
@@ -16,3 +16,13 @@ def pytest_report_teststatus(report):
             return "skipped", "s", "SKIPPED"
         else:
             return "positive", "SI", "correct"
+
+
+def tracefunc(frame, event, arg, indent=[0]):
+    if event == "call":
+        indent[0] += 2
+        print("-" * indent[0] + "> call function", frame.f_code.co_name)
+    elif event == "return":
+        print("<" + "-" * indent[0], "exit function", frame.f_code.co_name)
+        indent[0] -= 2
+    return tracefunc
