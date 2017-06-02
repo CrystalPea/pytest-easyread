feb7976387cec0db3245fd0dbb82b1cf4d27dac0
Pea Tyczynska <pea.tyczynska@digital.cabinet-office.gov.uk>
2017-06-02 16:29:25 +0100

    Rename file to better describe function

--- test_my_plugin.py
+++
@@ -1,7 +0,0 @@
-from conftest import is_it_answer_to_life_universe_and_everything
-
-def test_42_is_answer_to_life_universe_and_everything():
-    assert is_it_answer_to_life_universe_and_everything(42) == True
-
-def test_44is_not_answer_to_life_universe_and_everything():
-    assert is_it_answer_to_life_universe_and_everything("love") == False
