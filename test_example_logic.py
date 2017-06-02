9cce0fd9d351de0dac5c6a527060d85b6d5e7388
Pea Tyczynska <pea.tyczynska@digital.cabinet-office.gov.uk>
2017-06-02 16:28:40 +0100

    Rename file to better describe function

---
+++ test_example_logic.py
@@ -0,0 +1,7 @@
+from filetotest import is_it_answer_to_life_universe_and_everything
+
+def test_42_is_answer_to_life_universe_and_everything():
+    assert is_it_answer_to_life_universe_and_everything(42) == True
+
+def test_44_is_not_answer_to_life_universe_and_everything():
+    assert is_it_answer_to_life_universe_and_everything("love") == False
