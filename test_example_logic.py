c5bb8c2e30f1a7a6fea2162b494df06646846787
Pea Tyczynska <pea.tyczynska@digital.cabinet-office.gov.uk>
2017-06-09 17:18:27 +0100

    Add functions that are plugin is tested by

--- test_example_logic.py
+++ test_example_logic.py
@@ -5,3 +5,9 @@ def test_42_is_answer_to_life_universe_and_everything():
 
 def test_44_is_not_answer_to_life_universe_and_everything():
     assert is_it_answer_to_life_universe_and_everything("love") == False
+
+def test_failing_function():
+    assert 0
+
+def test_passing_function():
+    assert 1 == 1
