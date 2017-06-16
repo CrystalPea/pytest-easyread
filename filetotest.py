21326efb126b26c9de1541f777ca7769dd0fe6fe
Pea Tyczynska <pea.tyczynska@digital.cabinet-office.gov.uk>
2017-06-16 16:42:47 +0100

    Add example files to .gitignore

--- filetotest.py
+++
@@ -1,5 +0,0 @@
-def is_it_answer_to_life_universe_and_everything(integer):
-    if isinstance(integer, int):
-        return integer == 42
-    else:
-        raise NameError("Hint: it's a number! :P")
